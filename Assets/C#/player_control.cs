3fc848983169bcfb868f9687b7ea2b218405c729
Aillien <Allios2789@gmail.com>
2023-07-13 20:11:49 +0300

    mechanics from TheJoker2373

--- Assets/C#/player_control.cs
+++ Assets/C#/player_control.cs
@@ -1,207 +1,135 @@
 using System.Collections;
-using System.Collections.Generic;
-using Unity.PlasticSCM.Editor.WebApi;
+using Unity.VisualScripting;
 using UnityEngine;
-using UnityEngine.InputSystem;
 
 public class player_control : MonoBehaviour
 {
-  private InputSystem _input;
-  private Vector3 _input_direction;
-  private int _direction = 0;
-  private bool _jumped;
-  private bool _on_jump = false;
-  private float _jumped_distance = 0;
-  private Animator anim;
-  private float _speed;
+  private Rigidbody2D _rb;
+  private Vector2 _direction;
+  private Animator _anim;
+  private bool _is_sitting;
+  private bool _in_moved;
+
+  [SerializeField] private float _multiplier;
+  [SerializeField] private float _min_speed;
   private float _max_speed;
-  private float _min_speed;
+  private float _speed;
   private float _acceleration;
 
   private void Awake()
   {
-    _input = new InputSystem();
-  }
-
-  private states state
-  {
-    get { return (states) anim.GetInteger("state"); }
-    set { anim.SetInteger("state", (int)value); }
-  }
-
-  private void OnEnable()
-  {
-    _input.Enable();
-  }
-
-  private void OnDisable()
-  {
-    _input.Disable();
+    _rb = GetComponent<Rigidbody2D>();
+    _anim = GetComponent<Animator>();
   }
 
   private void Start()
   {
-    _min_speed = 0.6f;
     _acceleration = _min_speed;
     _speed = _min_speed;
-    _max_speed = _speed * 2;
-    anim = GetComponent<Animator>();
-  }
-
-  private void GetIdle(int direction)
-  {
-    switch (direction)
-    {
-      case 0:
-        state = states.idle;
-        GetComponent<SpriteRenderer>().flipX = false;
-        break;
-      case 1:
-        state = states.idle;
-        GetComponent<SpriteRenderer>().flipX = true;
-        break;
-      case 2:
-        state = states.idle_down;
-        break;
-      case 3:
-        state = states.idle_up;
-        break;
-    }
-  }
-
-  private void GetJump(int direction)
-  {
-    switch (direction)
-    {
-      case 0:
-        state = states.jump;
-        GetComponent<SpriteRenderer>().flipX = false;
-        break;
-      case 1:
-        state = states.jump;
-        GetComponent<SpriteRenderer>().flipX = true;
-        break;
-      case 2:
-        state = states.jump_down;
-        break;
-      case 3:
-        state = states.jump_up;
-        break;
-    }
-  }
-
-  private void GetMoved()
-  {
-    transform.position = Vector3.MoveTowards(transform.position, transform.position + _input_direction, _speed * Time.deltaTime);
-    if (_input_direction.x > 0)
-    {
-      state = states.walk;
-      GetComponent<SpriteRenderer>().flipX = false;
-      _direction = 0;
-    }
-    else if(_input_direction.x < 0)
-    {
-      state = states.walk;
-      GetComponent<SpriteRenderer>().flipX = true;
-      _direction = 1;
-    }
-    if(_input_direction.y < 0)
-    {
-      state = states.walk_down;
-      _direction = 2;
-    }
-    else if(_input_direction.y > 0)
-    {
-      state = states.walk_up;
-      _direction = 3;
-    }
-  }
-
-  private void JumpProcess()
-  {
-    if (!_on_jump)
-    {
-      if (_jumped_distance < 0.6f)
-      {
-        _jumped_distance += 0.10f;
-        transform.position = new Vector2(transform.position.x, transform.position.y + 0.10f);
-      }
-      else if (_jumped_distance >= 0.6f)
-        _on_jump = true;
-    }
-    else if (_on_jump)
-    {
-      if (_jumped_distance > 0f)
-      {
-        _jumped_distance -= 0.10f;
-        transform.position = new Vector2(transform.position.x, transform.position.y - 0.10f);
-      }
-      if (_jumped_distance <= 0f)
-      {
-        _on_jump = false;
-        _jumped = false;
-      }
-    }
-  }
-
-  private void FixedUpdate()
-  {
-    if(_jumped)
-    {
-      JumpProcess();
-    }
+    _max_speed = _speed * _multiplier;
   }
 
   private void Update()
   {
-    if (!_jumped && _input_direction == Vector3.zero) GetIdle(_direction);
-    if (_jumped) GetJump(_direction);
-    if (!_jumped && _input_direction != Vector3.zero) GetMoved();
-    _input.player.mover.performed += context => OnMover(context);
-    _input.player.mover.canceled += context => OffMover(context);
-    _input.player.jump.performed += context => OnJump(context);
+    if (_is_sitting)
+      StopPlayer();
+    else
+      SetDirection();
+    SetAnimations();
+    if (ChangeDirection())
+      Flip();
   }
 
-  private void OnJump(InputAction.CallbackContext context)
+  private void FixedUpdate()
   {
-    _jumped = true;
+    if (_direction != Vector2.zero && !_in_moved && !_is_sitting)
+    {
+      _speed = _min_speed;
+      StartCoroutine(AccelMove());
+      _in_moved = true;
+    }
+    MovePlayer();
   }
 
-  private void OnMover(InputAction.CallbackContext context)
+  private void SetDirection()
   {
-    StartCoroutine(AccelMove());
-    _input_direction = context.ReadValue<Vector2>();
+    float inputH = Input.GetAxisRaw("Horizontal");
+    float inputV = Input.GetAxisRaw("Vertical");
+
+    if (inputH != 0)
+    {
+      if (inputV == 0)
+        _direction = new Vector2(inputH, 0);
+    }
+    else if (inputV != 0)
+      _direction = new Vector2(0, inputV);
+    else
+      StopPlayer();
   }
 
-  private void OffMover(InputAction.CallbackContext context)
+  private bool ChangeDirection()
   {
-    _speed = 0f;
-    _input_direction = Vector2.zero;
+    if (transform.localScale.x > 0 && _direction.x < 0 || transform.localScale.x < 0 && _direction.x > 0)
+    {
+      return true;
+    }
+    else
+    {
+      return false;
+    }
   }
 
+  private void SetAnimations()
+  {
+    _anim.SetInteger("DirectionX", Mathf.RoundToInt(_direction.x));
+    _anim.SetInteger("DirectionY", Mathf.RoundToInt(_direction.y));
+    _anim.SetBool("isSitting", _is_sitting);
+  }
+
+  private void ResetAnimations()
+  {
+    _anim.SetInteger("DirectionX", 0);
+    _anim.SetInteger("DirectionY", 0);
+    _anim.SetBool("isSitting", false);
+  }
+
+  private void Flip()
+  {
+    transform.localScale = new Vector3(-transform.localScale.x, transform.localScale.y, transform.localScale.z);
+  }
+
+  private void StopPlayer()
+  {
+    _direction = Vector2.zero;
+  }
+
+  private void MovePlayer()
+  {
+    _rb.velocity = _direction * _speed;
+  }
 
   private IEnumerator AccelMove()
   {
     while (_speed <= _max_speed)
     {
-      yield return new WaitForSeconds(0.12f);
+      yield return new WaitForSeconds(0.15f);
       _speed += _acceleration;
+    }
+    if (_speed > _max_speed)
+    {
+      _speed = _max_speed;
+    }
+    if(_direction == Vector2.zero)
+    {
+      _speed = 0;
+      _in_moved = false;
     }
   }
 
-  enum states
+  public void Sit()
   {
-    idle,
-    idle_down,
-    idle_up,
-    jump,
-    jump_down,
-    jump_up,
-    sit,
-    sit_down,
-    sit_up,
-    walk,
-    walk_down,
-    walk_up
+    ResetAnimations();
+    _is_sitting = !_is_sitting;
   }
+
 }
---
+++ Assets/C#/seat.cs
@@ -0,0 +1,79 @@
+using System.Collections;
+using UnityEngine;
+
+public class seat : MonoBehaviour
+{
+  [SerializeField] private float _sit_speed = 3f;
+  [SerializeField] private bool _sit_direction;
+  private Animator _anim;
+  private player_control _player;
+  private Vector2 _start_pos;
+  private bool _is_sitting;
+
+  private void Update()
+  {
+    if (_player == null)
+      return;
+    if (Input.GetKeyDown(KeyCode.E))
+    {
+      if (_is_sitting)
+        //StartCoroutine(StandUp());
+        StandUp();
+      else
+        //StartCoroutine(SitDown());
+        SitDown();
+    }
+  }
+
+  private void OnTriggerStay2D(Collider2D collision)
+  {
+    if (_player == null)
+      _player = collision.GetComponent<player_control>();
+    if (_anim == null)
+      _anim = collision.GetComponent<Animator>();
+    _anim.SetBool("SitDirection", _sit_direction);
+  }
+
+  private void SitDown()
+  {
+    _is_sitting = true;
+    _player.GetComponent<Collider2D>().enabled = false;
+    _start_pos = _player.transform.position;
+    _player.Sit();
+    _player.transform.position = transform.position;
+  }
+
+  private void StandUp()
+  {
+    _player.transform.position = (Vector3)_start_pos;
+    _player.GetComponent<Collider2D>().enabled = true;
+    _player.Sit();
+    _is_sitting = false;
+    _player = null;
+  }
+
+  /*private IEnumerator SitDown()
+  {
+    _is_sitting = true;
+    _player.GetComponent<Collider2D>().enabled = false;
+    _start_pos = _player.transform.position;
+    _player.Sit();
+    while (_player.transform.position != transform.position)
+    {
+      _player.transform.position = Vector2.MoveTowards(_player.transform.position, transform.position, Time.deltaTime * _sit_speed);
+      yield return null;
+    }
+  }
+  private IEnumerator StandUp()
+  {
+    while (_player.transform.position != (Vector3)_start_pos)
+    {
+      _player.transform.position = Vector2.MoveTowards(_player.transform.position, _start_pos, Time.deltaTime * _sit_speed);
+      yield return null;
+    }
+    _player.GetComponent<Collider2D>().enabled = true;
+    _player.Sit();
+    _is_sitting = false;
+    _player = null;
+  }*/
+}
