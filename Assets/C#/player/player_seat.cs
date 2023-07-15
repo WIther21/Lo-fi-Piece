8edad177453a5fa22cc314d3be7296c151620dc8
Aillien <39222993+AILLIOS@users.noreply.github.com>
2023-07-15 22:30:51 +0300

    update player_seat

--- Assets/C#/player/player_seat.cs
+++ Assets/C#/player/player_seat.cs
@@ -3,7 +3,6 @@ using UnityEngine;
 
 public class player_seat : MonoBehaviour
 {
-  //[SerializeField] private float _sit_speed = 3f;
   [SerializeField] private bool _sit_direction;
   private Animator _anim;
   private player_control _player;
@@ -17,10 +16,8 @@ public class player_seat : MonoBehaviour
     if (Input.GetKeyDown(KeyCode.E))
     {
       if (_is_sitting)
-        //StartCoroutine(StandUp());
         StandUp();
       else
-        //StartCoroutine(SitDown());
         SitDown();
     }
   }
@@ -51,29 +48,4 @@ public class player_seat : MonoBehaviour
     _is_sitting = false;
     _player = null;
   }
-
-  /*private IEnumerator SitDown()
-  {
-    _is_sitting = true;
-    _player.GetComponent<Collider2D>().enabled = false;
-    _start_pos = _player.transform.position;
-    _player.Sit();
-    while (_player.transform.position != transform.position)
-    {
-      _player.transform.position = Vector2.MoveTowards(_player.transform.position, transform.position, Time.deltaTime * _sit_speed);
-      yield return null;
-    }
-  }
-  private IEnumerator StandUp()
-  {
-    while (_player.transform.position != (Vector3)_start_pos)
-    {
-      _player.transform.position = Vector2.MoveTowards(_player.transform.position, _start_pos, Time.deltaTime * _sit_speed);
-      yield return null;
-    }
-    _player.GetComponent<Collider2D>().enabled = true;
-    _player.Sit();
-    _is_sitting = false;
-    _player = null;
-  }*/
 }
